f72814be2e528ca03191eeb65aeeb6831f6b5434
Oliver Hamboerger <oliver.hamboerger@gmail.com>
2019-08-10 17:03:58 +0200

    Re-add test project

---
+++ Cvss.Suite.Tests/CvssSuite.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cvss.Suite.Tests
+{
+    [TestClass]
+    public class CvssSuite
+    {
+        [DataTestMethod]
+        [DataRow("Not a valid vector!")]
+        public void InvalidCvssVectors(string vector)
+        {
+            var cvss = Cvss.Suite.CvssSuite.Create(vector);
+
+            Assert.AreEqual(false, cvss.IsValid());
+            Assert.AreEqual(0, cvss.Version);
+        }
+
+    }
+}
